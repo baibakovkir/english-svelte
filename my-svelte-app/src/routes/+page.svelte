ba04ebcab93ec2e80626e3d91be20d7366616368
kiryxa09 <baibakovkir@yandex.ru>
2024-06-05 16:49:51 +0300

    feat: redesign of initial page

--- my-svelte-app/src/routes/+page.svelte
+++ my-svelte-app/src/routes/+page.svelte
@@ -1,24 +1,40 @@
 <script>
-    import englishColor from '$lib/assets/ninja_writes.jpg';
-    import spanishColor from '$lib/assets/wninja_writes.jpg';
-    import codingColor from '$lib/assets/wninja_coder.jpg';
+  import englishColor from '$lib/assets/english.avif';
+  import spanishColor from '$lib/assets/spanishFlag.avif';
+  import codingColor from '$lib/assets/programming.avif';
+
+    
+  import { form, field } from 'svelte-forms';
+  import { required } from 'svelte-forms/validators';
+
+  let name = field('name', '', [required()]);
+  let message = field('message', '', [required()]);
+  const myForm = form(name, message);
 </script>
 <section class="initial">
 <h1 class="initial__title">
-  Изучаем английский вместе!
+  База знаний
 </h1>
+<p class="initial__text">База знаний - это платформа для изучения всего на свете</p>
+<p class="initial__text">На данный момент доступны курсы:</p>
 <div class="initial__grid">
   <div class="initial__box">
     <img src="{englishColor}" alt="Английский" class="initial__box__img">
-    <p class="initial__box__text">Английский язык</p>
+    <p class="initial__box__text initial__box__text_yellow">Английский язык</p>
   </div>
   <div class="initial__box">
     <img src="{spanishColor}" alt="Испанский" class="initial__box__img">
-    <p class="initial__box__text">Испанский язык</p>
+    <p class="initial__box__text initial__box__text_yellow">Испанский язык</p>
   </div>
   <div class="initial__box">
     <img src="{codingColor}" alt="Программирование" class="initial__box__img">
-    <p class="initial__box__text">Программирование</p>
+    <p class="initial__box__text initial__box__text_yellow">Программирование</p>
   </div>
 </div>
+</section>
+<section class="spheres">
+  <h2 class="spheres__text">Связаться с разработчиками</h2>
+    <input class="spheres__input" type="text" bind:value={name} placeholder="Ваше имя">
+    <input class="spheres__input" type="text" bind:value={message} placeholder="Ваше сообщение">
+    <button class="spheres__button">Отправить</button>
 </section>
