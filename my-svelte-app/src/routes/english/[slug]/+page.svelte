873fa29de6e097c807404a64e4282ee7eec6cc58
kiryxa09 <baibakovkir@yandex.ru>
2024-07-04 16:48:22 +0300

    feat: added material

--- my-svelte-app/src/routes/english/[slug]/+page.svelte
+++ my-svelte-app/src/routes/english/[slug]/+page.svelte
@@ -2,13 +2,24 @@
   export let data;
 </script>
 
-<section class="english__course">
-  <h1 class="english__course__title">{data.post.title}</h1>
-  <div class="english__course__container">
-    <ul>
-      {#each data.post.content as { name, slug }}
-        <li><a href="/english/{slug}">{name}</a></li>
-      {/each}
-    </ul>
+<section class="english-course">
+  <h1 class="english-course__title">{data.post.title}</h1>
+  <div class="english-course__container">
+    <div class="english-course__menu english-course__menu_left">
+      <h2 class="english-course__subtitle">Необходимые знания:</h2>
+      <div class="english-course__list-box">
+        <ul class="english-course__list">
+          {#each data.post.content as { name, slug }}
+          <a class="english-course__link" href="/grammar/{slug}"><li class="english-course__item">{name}</li></a>
+          {/each}
+        </ul>
+      </div>
+    </div>
+    <div class="english-course__menu english-course__menu_right">
+      <p class="english-course__text">Выберите нужную тему. В каждой теме есть теория и практические задания. Также в темах будут задания на грамматику, аудирование, работа с текстом и видео.</p>
+      {#if data.post.description}
+        <p class="english-course__text">{data.post.description}</p>
+      {/if}
+    </div>
   </div>
 </section>
